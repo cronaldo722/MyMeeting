cd2bde818a4e48faeb4b4beb1496d86190f2d123
cronaldo722 <2523179247@qq.com>
2019-12-13 10:33:07 +0800

    优化MeetingService

--- src/cn/edu/ncu/Service/Impl/MeetingServiceImpl.java
+++ src/cn/edu/ncu/Service/Impl/MeetingServiceImpl.java
@@ -9,40 +9,35 @@ import cn.edu.ncu.vo.User;
 import java.util.List;
 
 public class MeetingServiceImpl implements IMeetingService {
+    private MeetingDao meetingDao=new MeetingImpl();
 
     @Override
     public  boolean addMeeting(Meeting meeting) {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.addMeeting(meeting);
     }
 
     @Override
     public Meeting getMeetingById(String idmeeting) {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.getMeetingById(idmeeting);
     }
 
     @Override
     public List<Meeting> getAllMeeting() {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.getAllMeeting();
     }
 
     @Override
     public List<User> getMeetingUsers(String idmeeting) {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.getMeetingUsers(idmeeting);
     }
 
     @Override
     public boolean AttendMeeingById(String idmeeting,String name) {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.AttendMeetingById(idmeeting,name);
     }
 
     @Override
     public boolean UpdataMeetingById(String idmeeting) {
-        MeetingDao meetingDao=new MeetingImpl();
         return meetingDao.UpdataMeetingById(idmeeting);
     }
 
